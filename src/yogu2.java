733ce013b15cfe7b36bd23b7ea594080c23f486a
msi <msi@DESKTOP-8HHQCE2>
2021-08-10 16:45:19 +0900

    JAVA 플레이그라운드 구구단 최종요구사항2

---
+++ src/yogu2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class yogu2 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("출력하기 원하는 구구단 값을 입력하세요.(예 : 2,3)");
+		Scanner scanner = new Scanner(System.in);
+		String inputValue = scanner.nextLine();
+		
+		String[] splitedValue = inputValue.split(",");
+		
+		int first = Integer.parseInt(splitedValue[0]);
+		int second = Integer.parseInt(splitedValue[1]);
+		
+		int[] result = new int[second];
+		
+		
+ 			for(int j =2; j< first+1; j++) {
+ 				
+ 				System.out.println(j+"단");
+ 				
+ 				for(int i = 0; i < result.length; i++) {
+ 					
+ 					result[i] = j * (i + 1);
+ 					
+ 					System.out.println(j+"*"+(i+1)+"="+result[i]);
+		
+ 					}
+ 				System.out.println();
+ 		}
+	}
+}
